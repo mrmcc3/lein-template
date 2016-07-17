ad3b1786f5cc47e2cf509ac2d31b7c78878bd368
Michael McClintock <mrmcc3@gmail.com>
2016-07-17 11:21:53 +1000

    fixed project description and url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject mrmcc3/lein-template "0.1.0"
+(defproject mrmcc3/lein-template "0.1.1"
   :description "there are many lein templates. this one is mine"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/mrmcc3/lein-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
